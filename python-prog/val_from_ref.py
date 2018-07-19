3a298dac25126d860749008e40d88fe5470ff5a1
Deepak <Deepak0101@users.noreply.github.com>
2018-07-19 18:31:10 +0530

    Print values from other files
    This program used string_print.py file and uses the value mentioned in that.

---
+++ python-prog/val_from_ref.py
@@ -0,0 +1,19 @@
+# is used for commenting
+
+# this is my first python script for training
+
+import string_print
+#from ex02 import author_name,author_email :--> will load only the required feilds from ex02 
+
+from string_print import author_name as name,autor_city as email
+print('hello world', end = ' _ ')
+print('Helooo how are you ')
+
+
+
+print('Name = ',string_print.author_name)
+print(string_print.autor_city)
+
+
+print('Name = ', name)
+print(string_print.autor_city)
